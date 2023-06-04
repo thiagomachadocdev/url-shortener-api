0d8e25b5e9cf0556d5822919ba4fa3d6ab03e75f
Thiago Machado <contatocostathiago@gmail.com>
2023-06-04 19:09:50 -0300

    feat: adds cors

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import controller from './src/controller'
+import cors from 'cors'
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors())
 
 app.post('/shorten-url', controller)
 
